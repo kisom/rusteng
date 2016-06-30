e32f3ac414e228909c081e551866a5e65e797c4f
Kyle Isom <coder@kyleisom.net>
2016-06-29 23:48:50 -0700

    More getopt stuff.

--- kvdemo/src/main.rs
+++ kvdemo/src/main.rs
@@ -3,11 +3,11 @@ use getopts::Options;
 use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<_> = env::args().collect();
     let mut opts = Options::new();
     opts.optopt("a", "", "Address server should listen on.", "ADDRESS");
+    opts.optopt("f", "", "Path to disk store.", "FILE");    
     opts.optflag("h", "help", "Print a short usage message.");
-    opts.optopt("f", "", "Path to disk store.", "FILE");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
@@ -15,8 +15,24 @@ fn main() {
     };
 
     if matches.opt_present("h") {
-        let brief = format!("Usage: {} FILE [options]", args[0]);        
+        let brief = format!("Usage: {} [options]", args[0]);        
         print!("{}", opts.usage(&brief));
         return;
+    }
+
+    let mut addr = "localhost:8000";
+    if matches.opt_present("a") {
+        match matches.opt_str("a") {
+            Some(a) => { addr = a; }
+            None    => { panic!("address argument present but unavailable."); }
+        };
+    }
+
+    let mut store_file = "store.json";
+    if matches.opt_present("f") {
+        match matches.opt_str("f") {
+            Some(f) => { store_file = f; }
+            None    => { panic!("store file argument present but unavailable."); }
+        };
     }
 }
